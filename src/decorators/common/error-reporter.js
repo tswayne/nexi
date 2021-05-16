5bb6ac0cc483f0ace8a17ba9426acb273189541c
Tyler Swayne <swaynetyler@gmail.com>
2021-05-16 16:01:06 -0700

    fix error-reporter require path

--- src/decorators/common/error-reporter.js
+++ src/decorators/common/error-reporter.js
@@ -13,7 +13,7 @@ const defaultReporter = {
 }
 
 const errorReporter = (config, overrides={}) => {
-  const filePath = path.join(config.rootDir, `config/error-reporter.js`)
+  const filePath = path.join(config.srcDir, `config/error-reporter.js`)
   if (fs.existsSync(filePath)) {
     const reporter = require(filePath)(config)
     if (reporter) {
