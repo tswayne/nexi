55c188c1dbf99033f71f091638474a6118ae64c7
Tyler Swayne <swaynetyler@gmail.com>
2023-05-17 19:01:20 -0700

    small code shuffle

---
+++ src/bootstrap.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const fs = require('fs')
+if (parseInt(process.versions.node.split('.')[0]) >= 18) {
+    const dns = require('dns');
+    dns.setDefaultResultOrder('ipv4first');
+}
+
+const srcDir = path.join(process.cwd(), 'src')
+const includesPath = path.join(srcDir, 'config/includes.js')
+if (fs.existsSync(includesPath)) { require(includesPath) }
+
+const config = require('./config')(srcDir)
+const logger = require('./logging/logger')(config)
+
+const decorate = require('./decorators/decorator')
+const initialize = require('./initializers/initializer')
+
+const bootstrap = async () => {
+    const context = await decorate(config, logger)
+    await initialize(context)
+    return context
+}
+
+module.exports = bootstrap
--- src/cli.js
+++ src/cli.js
@@ -1,25 +1,4 @@
-const path = require('path')
+const bootstrap = require('./bootstrap')
 const repl = require('repl')
-const fs = require('fs')
-if (parseInt(process.versions.node.split('.')[0]) >= 18) {
-  const dns = require('dns');
-  dns.setDefaultResultOrder('ipv4first');
-}
 
-const srcDir = path.join(process.cwd(), 'src')
-const includesPath = path.join(srcDir, 'config/includes.js')
-if (fs.existsSync(includesPath)) { require(includesPath) }
-
-const config = require('./config')(srcDir)
-const logger = require('./logging/logger')(config)
-
-const decorate = require('./decorators/decorator')
-const initialize = require('./initializers/initializer')
-const dns = require('dns')
-
-decorate(config, logger)
-    .then(async c => {
-        await initialize(c)
-        return c
-    })
-    .then(c => repl.start('> ').context.context = c).catch(console.error)
+bootstrap().then(c => repl.start('> ').context.context = c).catch(console.error)
